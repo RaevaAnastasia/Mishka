572dc8c30978ec7139b99a6453313fbcd2739494
Keks <keks@htmlacademy.ru>
2019-04-28 20:18:40 +0300

    :heavy_check_mark: Сборка #9

---
+++ 9/js/script.js
@@ -0,0 +1,42 @@
+var mainNavigation = document.querySelector(".main-navigation");
+var navToggle = document.querySelector(".main-navigation__toggle");
+var modalUp = document.querySelectorAll(".modal-up");
+var modal = document.querySelector(".modal");
+var modalOverlay = document.querySelector(".modal-overlay");
+
+mainNavigation.classList.remove("main-navigation--nojs");
+
+navToggle.addEventListener("click", function() {
+  if (mainNavigation.classList.contains("main-navigation--closed")) {
+    mainNavigation.classList.remove("main-navigation--closed");
+    mainNavigation.classList.add("main-navigation--opened");
+  } else {
+    mainNavigation.classList.add("main-navigation--closed");
+    mainNavigation.classList.remove("main-navigation--opened")
+  }
+});
+
+for (var i = 0; i < modalUp.length; i++) {
+  modalUp[i].addEventListener("click", function(evt) {
+    evt.preventDefault();
+    modal.classList.add("modal--show");
+    modalOverlay.classList.add("modal-overlay--show");
+  });
+}
+
+modalOverlay.addEventListener("click", function(evt) {
+    evt.preventDefault();
+    modal.classList.remove("modal--show");
+    modalOverlay.classList.remove("modal-overlay--show");
+});
+
+window.addEventListener("keydown", function(evt) {
+  if (evt.keyCode === 27) {
+    evt.preventDefault();
+    if (modal.classList.contains("modal--show")) {
+      evt.preventDefault();
+      modal.classList.remove("modal--show");
+      modalOverlay.classList.remove("modal-overlay--show");
+    }
+  }
+});
